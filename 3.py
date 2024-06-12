09773781e6e40d1462805c3c1249edd0b72ab2a2
Eric Tran <tadakatsu213@hotmail.com>
2024-06-13 01:38:11 +1000

    finish

--- 3.py
+++ 3.py
@@ -1,16 +1,14 @@
-import csv
 import os
+import csv
+csv_path = 'PyBank/Resources/budget_data.csv'
 
-budget_data = os.path.join("Pybank", "Resources", "budget_data.csv")
+with open(csv_path) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    print(csvreader)
+    csv_header = next(csvreader)
+    print(f"csv_header: {csv_header}")
 
-with open(budget_data) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter = ",")
-
-csv_header = next(csv_file)
-print(f"Header: {csv_header}")
-
+    for row in csvreader:
+        print(row)
 Total_months = 0
 total_profit_losses = 0
-
-
-
