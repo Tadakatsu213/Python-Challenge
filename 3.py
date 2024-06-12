307ec020a5ef9c7bd796211cb47ffa8993699ec1
Eric Tran <tadakatsu213@hotmail.com>
2024-06-12 17:53:07 +1000

    hi

--- 3.py
+++ 3.py
@@ -1,6 +1,13 @@
 import csv
+import os
 
-csv_path = 'budget_data.csv'
+budget_data = os.path.join("Pybank", "Resources", "budget_data.csv")
+
+with open(budget_data) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ",")
+
+csv_header = next(csv_file)
+print(f"Header: {csv_header}")
 
 Total_months = 0
 total_profit_losses = 0
